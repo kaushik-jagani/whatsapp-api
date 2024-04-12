2d3f26ef5fec9436ddbe9df1a7f7223239853fdf
kaushikwork2411 <kaushikwork2411@gmail.com>
2024-04-13 00:37:51 +0530

    change db

--- controller/sessionController.js
+++ controller/sessionController.js
@@ -20,7 +20,7 @@ let store;
 mongoose.connect('mongodb://127.0.0.1:27017/RefactorDB')
 .then(() => {
     store = new MongoStore({ mongoose });
-})
+}) 
 .catch(err => {
     console.log('Failed to connect to MongoDB:', err);
     process.exit(1);
